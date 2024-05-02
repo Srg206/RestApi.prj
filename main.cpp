ca87e0267e41a3ca9b2d9048d6145ff0fc60a98b
Srg206 <Srg206.32@gmail.com>
2024-05-02 23:32:25 +0300

    upd

--- main.cpp
+++ main.cpp
@@ -2,16 +2,24 @@
 //#include <boost/asio.hpp>
 
 
-#include<fstream>
+//#include<fstream>
+//#include<iostream>
+//#include<string>
+
+
 
 int main()
 {
     //Server a("127.0.0.1", 8000);
     //a.Work();
-    std::ofstream f("fake_db.txt");
-    std::cout<<f.is_open()<<"nnnnnnnn";
-    f<<"mmvmvmmvd";
+    std::ofstream f("resources/fake_db.txt");
+    f<<"mвалmmиььи\n\n\n\n\nn\nvmmvd\n"<<std::endl;
+    f.close();
+    std::ifstream f2("resources/fake_db.txt");
+    std::string str;
+    f2>>str;
 
-    // Дальнейший код
+    std::cout<<str;
+    f2.close();
     return 0;
 }
