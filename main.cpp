0041f3d508dd51f0a5da7af45eeb552db7668d93
Srg206 <Srg206.32@gmail.com>
2024-05-06 09:38:09 +0300

    upd

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
-// #include <server/server.hpp>
+#include <server/server.hpp>
 #include <iostream>
 
 #include <pqxx/pqxx>
-#include <postgresql/libpq-fe.h>
+//#include <postgresql/libpq-fe.h>
 //#include <libpq-fe.h>
 using namespace pqxx;
 using namespace std;
@@ -10,8 +10,8 @@ using namespace std;
 int main()
 {
     std::cout << "Docker is working" << std::endl;
-    // Server a("127.0.0.1", 8000);
-    // a.Work();
+    Server a("127.0.0.1", 8000);
+     a.Work();
     // connection C("dbname=test-db user=user password=user123 hostaddr=172.16.63.8 port=5432");
     connection C{"postgres://user:user123@172.16.63.8/test-db"}; //"postgresql://user:password@localhost/dbname"
     //C.disconnect();
